e76b8c1a8ac3277e2982de1f80c607eea93462ab
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 06:46:38 +0530

    Nested while loop

---
+++ NestedWhileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NestedWhileLoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //intializing first variable
+            int a = 0;
+            //first loop with first variable
+            while (a < 2)
+            {
+                Console.WriteLine("Value of a: {0}", a);
+                //intializing Second variable
+                int b = 1;
+
+                a++;
+                //Second loop with Second variable
+                while (b < 2)
+                {
+                    Console.WriteLine("Value of b: {0}", b);
+                    b++;
+                }
+            }
+        }
+    }
+}
